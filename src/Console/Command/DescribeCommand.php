15eeb62a08c4c291eb0e8104d3f0f5c01e612d0a
SpacePossum <possumfromspace@gmail.com>
2016-11-21 09:52:59 +0100

    DescribeCommand - Add more details on failure

--- src/Console/Command/DescribeCommand.php
+++ src/Console/Command/DescribeCommand.php
@@ -34,6 +34,16 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class DescribeCommand extends Command
 {
     /**
+     * @var string[]
+     */
+    private $setNames;
+
+    /**
+     * @var array<string, FixerInterface>
+     */
+    private $fixers;
+
+    /**
      * {@inheritdoc}
      */
     protected function configure()
@@ -55,25 +65,36 @@ final class DescribeCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $name = $input->getArgument('name');
+        try {
+            if ('@' === $name[0]) {
+                $this->describeSet($input, $output, $name);
 
-        if ('@' === substr($name, 0, 1)) {
-            $this->describeSet($input, $output, $name);
-        } else {
+                return;
+            }
+
             $this->describeRule($input, $output, $name);
+        } catch (DescribeNameNotFoundException $e) {
+            $alternative = $this->getAlternative($e->getType(), $name);
+            $this->describeList($input, $output, $e->getType());
+
+            throw new \InvalidArgumentException(sprintf(
+                '%s %s not found.%s',
+                ucfirst($e->getType()), $name, null === $alternative ? '' : ' Did you mean "'.$alternative.'"?'
+            ));
         }
     }
 
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     * @param string          $name
+     */
     private function describeRule(InputInterface $input, OutputInterface $output, $name)
     {
-        $fixerFactory = new FixerFactory();
-        $fixers = array();
-
-        foreach ($fixerFactory->registerBuiltInFixers()->getFixers() as $fixer) {
-            $fixers[$fixer->getName()] = $fixer;
-        }
+        $fixers = $this->getFixers();
 
         if (!isset($fixers[$name])) {
-            throw new \InvalidArgumentException(sprintf('Rule "%s" does not exist.', $name));
+            throw new DescribeNameNotFoundException($name, 'rule');
         }
 
         $fixer = $fixers[$name];
@@ -142,25 +163,28 @@ final class DescribeCommand extends Command
         }
     }
 
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     * @param string          $name
+     */
     private function describeSet(InputInterface $input, OutputInterface $output, $name)
     {
+        if (!in_array($name, $this->getSetNames(), true)) {
+            throw new DescribeNameNotFoundException($name, 'set');
+        }
+
         $ruleSet = new RuleSet(array($name => true));
         $rules = $ruleSet->getRules();
         ksort($rules);
 
-        $fixerFactory = new FixerFactory();
-        $fixers = array();
-
-        foreach ($fixerFactory->registerBuiltInFixers()->getFixers() as $fixer) {
-            $fixers[$fixer->getName()] = $fixer;
-        }
+        $fixers = $this->getFixers();
 
         $output->writeln(sprintf('<info>Description of %s set.</info>', $name));
         $output->writeln('');
 
         $help = '';
 
-        $count = count($rules) - 1;
         foreach ($rules as $rule => $config) {
             $help .= sprintf(
                 " * <info>%s</info>%s\n   | %s\n%s\n",
@@ -211,5 +235,92 @@ final class DescribeCommand extends Command
             $replaces[1],
             var_export($data, true)
         );
+    }
+
+    /**
+     * @return array<string, FixerInterface>
+     */
+    private function getFixers()
+    {
+        if (null !== $this->fixers) {
+            return $this->fixers;
+        }
+
+        $fixerFactory = new FixerFactory();
+        $fixers = array();
+
+        foreach ($fixerFactory->registerBuiltInFixers()->getFixers() as $fixer) {
+            $fixers[$fixer->getName()] = $fixer;
+        }
+
+        $this->fixers = $fixers;
+        ksort($this->fixers);
+
+        return $this->fixers;
+    }
+
+    /**
+     * @return string[]
+     */
+    private function getSetNames()
+    {
+        if (null !== $this->setNames) {
+            return $this->setNames;
+        }
+
+        $set = new RuleSet();
+        $this->setNames = $set->getSetDefinitionNames();
+        sort($this->setNames);
+
+        return $this->setNames;
+    }
+
+    /**
+     * @param string $type 'rule'|'set'
+     * @param string $name
+     *
+     * @return null|string
+     */
+    private function getAlternative($type, $name)
+    {
+        $other = null;
+        $alternatives = 'set' === $type ? $this->getSetNames() : $this->getFixers();
+
+        foreach ($alternatives as $alternative) {
+            $distance = levenshtein($name, $alternative);
+            if (3 > $distance) {
+                $other = $alternative;
+
+                break;
+            }
+        }
+
+        return $other;
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     * @param string          $type   'rule'|'set'
+     */
+    private function describeList(InputInterface $input, OutputInterface $output, $type)
+    {
+        if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERY_VERBOSE) {
+            $describe = array(
+                'set' => $this->getSetNames(),
+                'rules' => $this->getFixers(),
+            );
+        } elseif ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
+            $describe = 'set' === $type ? array('set' => $this->getSetNames()) : array('rules' => $this->getFixers());
+        } else {
+            return;
+        }
+
+        foreach ($describe as $list => $items) {
+            $output->writeln(sprintf('<comment>Defined %s:</comment>', $list));
+            foreach ($items as $name => $item) {
+                $output->writeln(sprintf('- %s', is_string($name) ? $name : $item));
+            }
+        }
     }
 }
